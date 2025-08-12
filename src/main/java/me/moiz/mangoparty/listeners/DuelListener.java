e348fb1c15cae0c29129c59088549016a54ec38e
Moizdev-ops <muhammadmoizamjad@gmail.com>
2025-08-12 10:20:23 +0500

    asdf

--- src/main/java/me/moiz/mangoparty/listeners/DuelListener.java
+++ src/main/java/me/moiz/mangoparty/listeners/DuelListener.java
@@ -6,6 +6,7 @@ import me.moiz.mangoparty.models.Duel;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.EntityEffect;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
